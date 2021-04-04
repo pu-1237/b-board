e6ba5dabf9df678850213baef7b6ebb9907c9879
pu-1237 <puchiko1237@gmail.com>
2021-04-04 21:52:43 +0900

    second commit

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,13 +1,28 @@
 class TopicsController < ApplicationController
   def index
+    @topics = Topic.all
   end
 
   def show
+    @topic = Topic.find(params[:id])
   end
 
   def new
+    @topic = Topic.new
   end
 
   def edit
+  end
+
+  def create
+    topic = Topic.new(topic_params)
+    topic.save!
+    redirect_to topics_url, notice: "トピック「#{topic.title}」を投稿しました。"
+  end
+
+  private
+  
+  def topic_params
+    params.require(:topic).permit(:title, :body)
   end
 end
